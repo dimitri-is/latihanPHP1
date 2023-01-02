29cff470447fe1f547ba5f79c5db43b570822fd2
Dimitri Irfan <dimitriirfan08@gmail.com>
2023-01-02 22:33:39 +0800

    telah menambahkan folder pembelajar PHP IDE NetBeans menggunakan XAMPP

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>latihan PHP 1</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        echo 'Di PHP, kamu dapat melakukan memberikan perintah echo.';
+        print "atau menggunakan perintah print, seperti pada sebagian bahasa pemrograman"
+        ?>
+    </body>
+</html>
